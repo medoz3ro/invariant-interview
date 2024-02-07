e01a8e8d0d0fb79821af7e00b9d0ff34b65cee24
medoz3ro <bsabo44@gmail.com>
2024-02-08 00:41:11 +0100

    fix: edit notes - cancel button

--- Invariant shop/Invariant shop/View/EditeNoteView.swift
+++ Invariant shop/Invariant shop/View/EditeNoteView.swift
@@ -1,16 +1,32 @@
 import SwiftUI
 
+enum ActiveAlert: Identifiable {
+    case discardChanges, deleteConfirmation
+    
+    // Conform to Identifiable
+    var id: String {
+        switch self {
+        case .discardChanges:
+            return "discardChanges"
+        case .deleteConfirmation:
+            return "deleteConfirmation"
+        }
+    }
+}
+    
+
 struct EditNoteView: View {
     @Environment(\.presentationMode) var presentationMode
     var onSave: (Note) -> Void
     var onDelete: (Note) -> Void
     
+
+    
     @Binding var note: Note
     @State private var title: String
     @State private var noteContent: String
     @State private var linkedItemIDs: [UUID]
-    @State private var showingDiscardAlert = false
-    @State private var showingDeleteAlert = false
+    @State private var activeAlert: ActiveAlert?
     @State private var isShowingItemPicker = false
     
     @State private var items: [Item] = []
@@ -37,38 +53,49 @@ struct EditNoteView: View {
                         isShowingItemPicker = true
                     }
                 }
+                
+                Section {
+                    Button("Delete Note", role: .destructive) {
+                        activeAlert = .deleteConfirmation
+                    }
+                }
             }
             .navigationTitle("Edit Note")
             .navigationBarItems(leading: Button("Cancel") {
-                if hasChanges {
-                    showingDiscardAlert = true
+                if hasChanges() {
+                    activeAlert = .discardChanges
                 } else {
                     presentationMode.wrappedValue.dismiss()
                 }
             }, trailing: Button("Save") {
-                saveNote()
+                if validateInput() {
+                    saveNote()
+                }
             })
-            .alert(isPresented: $showingDiscardAlert) {
-                Alert(
-                    title: Text("Discard Changes?"),
-                    message: Text("Are you sure you want to discard your changes?"),
-                    primaryButton: .destructive(Text("Discard")) {
-                        presentationMode.wrappedValue.dismiss()
-                    },
-                    secondaryButton: .cancel()
-                )
+            .alert(item: $activeAlert) { alertType in
+                switch alertType {
+                case .discardChanges:
+                    return Alert(
+                        title: Text("Discard Changes?"),
+                        message: Text("Are you sure you want to discard your changes?"),
+                        primaryButton: .destructive(Text("Discard")) {
+                            presentationMode.wrappedValue.dismiss()
+                        },
+                        secondaryButton: .cancel()
+                    )
+                case .deleteConfirmation:
+                    return Alert(
+                        title: Text("Confirm Delete"),
+                        message: Text("Are you sure you want to delete this note?"),
+                        primaryButton: .destructive(Text("Delete")) {
+                            onDelete(note)
+                            presentationMode.wrappedValue.dismiss()
+                        },
+                        secondaryButton: .cancel()
+                    )
+                }
             }
-            .alert(isPresented: $showingDeleteAlert) {
-                Alert(
-                    title: Text("Confirm Delete"),
-                    message: Text("Are you sure you want to delete this note?"),
-                    primaryButton: .destructive(Text("Delete")) {
-                        onDelete(note)
-                        presentationMode.wrappedValue.dismiss()
-                    },
-                    secondaryButton: .cancel()
-                )
-            }
+
             .sheet(isPresented: $isShowingItemPicker) {
                 ItemPickerView(linkedItemIDs: $linkedItemIDs)
             }
@@ -78,7 +105,7 @@ struct EditNoteView: View {
         }
     }
     
-    private var hasChanges: Bool {
+    private func hasChanges() -> Bool {
         return note.title != title || note.note != noteContent || note.linkedItemIDs != linkedItemIDs
     }
     
@@ -87,9 +114,15 @@ struct EditNoteView: View {
         onSave(updatedNote)
         presentationMode.wrappedValue.dismiss()
     }
+    
+    private func validateInput() -> Bool {
+        if title.isEmpty {
+            print("Title cannot be empty.")
+            return false
+        }
+        return true
+    }
 }
-
-
 
 struct EditNoteView_Previews: PreviewProvider {
     static var previews: some View {
