9c69c515469a27bdcc4422b51fc86c184a560cba
medoz3ro <bsabo44@gmail.com>
2024-02-08 09:28:23 +0100

    feat: item picker: save button

--- Invariant shop/Invariant shop/View/ItemPickerView.swift
+++ Invariant shop/Invariant shop/View/ItemPickerView.swift
@@ -3,19 +3,19 @@ import SwiftUI
 struct ItemPickerView: View {
     @Environment(\.presentationMode) var presentationMode
     @Binding var linkedItemIDs: [UUID] // IDs of linked items passed from the parent view
-    
+
     @State private var filterText: String = ""
     @State private var items: [Item] = [] // This will be fetched from DataManager
-    
+
     private let dataManager = DataManager()
-    
+
     var body: some View {
         NavigationView {
             VStack {
                 TextField("Filter items by name", text: $filterText)
                     .padding()
                     .textFieldStyle(RoundedBorderTextFieldStyle())
-                
+
                 List {
                     ForEach(filteredItems, id: \.id) { item in
                         HStack {
@@ -36,13 +36,16 @@ struct ItemPickerView: View {
             .navigationBarTitle("Select Items", displayMode: .inline)
             .navigationBarItems(leading: Button("Back") {
                 presentationMode.wrappedValue.dismiss()
+            }, trailing: Button("Save") {
+                // Action to save the selected items or simply dismiss the view if saving is handled elsewhere
+                presentationMode.wrappedValue.dismiss()
             })
             .onAppear {
                 items = dataManager.loadItems()
             }
         }
     }
-    
+
     private var filteredItems: [Item] {
         if filterText.isEmpty {
             return items
@@ -50,7 +53,7 @@ struct ItemPickerView: View {
             return items.filter { $0.name.lowercased().contains(filterText.lowercased()) }
         }
     }
-    
+
     private func toggleItemSelection(item: Item) {
         if let index = linkedItemIDs.firstIndex(of: item.id) {
             linkedItemIDs.remove(at: index)
