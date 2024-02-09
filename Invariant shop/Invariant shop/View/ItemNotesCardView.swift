b543b6e3271fab23b20ac82cfe7c8c9daa6fd4c7
medoz3ro <bsabo44@gmail.com>
2024-02-09 20:44:34 +0100

    feat: linked items

--- Invariant shop/Invariant shop/View/ItemNotesCardView.swift
+++ Invariant shop/Invariant shop/View/ItemNotesCardView.swift
@@ -7,45 +7,56 @@
 
 import SwiftUI
 
+import SwiftUI
+
 struct ItemCardNotesView: View {
     var note: Note
-    
+
     private var dateFormatter: DateFormatter {
         let formatter = DateFormatter()
         formatter.dateStyle = .medium
         formatter.timeStyle = .short
         return formatter
     }
-    
+
     var body: some View {
         VStack(alignment: .leading, spacing: 5) {
             Text(note.title)
-                .textStyle()
-            
+                .font(.headline) // Uses headline font for the title
+
             HStack {
                 Text("Note:")
                 Text(note.note?.isEmpty ?? true ? "Empty" : note.note!)
+                    .font(.subheadline) // Subheadline font for note content
             }
-            .textStyle(font: .subheadline)
-            
-            
+
+            HStack {
+                Text("Linked Items:")
+                Text("\(note.linkedItemIDs.count)")
+                    .font(.subheadline) // Subheadline font for linked items count
+            }
+
             Spacer()
-            
+
             HStack {
                 Spacer()
                 Text("Created: \(dateFormatter.string(from: note.creationDate))")
                     .font(.caption)
-                    .foregroundColor(.gray)
+                    .foregroundColor(.gray) // Gray color for the creation date
             }
         }
-        .cardStyle()
+        .padding() // Adds padding inside the card
+        .background(Color.white) // Sets the card's background color to white
+        .cornerRadius(10) // Rounds the corners of the card
+        .shadow(radius: 5) // Adds a shadow for a slight depth effect
     }
 }
 
 struct ItemCardNotesView_Previews: PreviewProvider {
     static var previews: some View {
         let sampleNote = Note(id: UUID(), title: "Sample Title", note: "Sample Note Content", linkedItemIDs: [], creationDate: Date())
-        
+
         ItemCardNotesView(note: sampleNote)
+            .previewLayout(.sizeThatFits) // Adjusts the preview to fit the content
     }
 }
